57d09e8cf4ef42d985d8c1a8711a73284f34d733
Adrian <amobley85@gmail.com>
2015-12-03 13:30:53 -0500

    Update warmUp.js

--- warmUp.js
+++ warmUp.js
@@ -1,39 +1,34 @@
 function my_max (numberArray) {
 		
-			if(!(numberArray instanceof Array)) {
-					return "Please enter an array";
-				}
-			var arrayLength = numberArray.length,
-			largest = numberArray[0];
+  if(!(numberArray instanceof Array)) {
+	return "Please enter an array";
+  }
+  var arrayLength = numberArray.length,
+  largest = numberArray[0];
 			
+  for(var i = 0; i < arrayLength; i += 1) {
+    if (largest < numberArray[i]) {
+	largest = numberArray[i];
+	}
+  }
+  
+  return largest;
+	
+}
+			
+function vowel_count(myString) {
+  var count = 0,
+  stringLength = myString.length;
+  vowels = /[aeiou]/i;
 				
-				
-				for(var i = 0; i < arrayLength; i += 1) {
-					
-					if (largest < numberArray[i]) {
-						largest = numberArray[i];
-					}
-				}
-				return largest;
-			}
+  for(var i = 0; i < stringLength; i += 1) {
+    if(vowels.test(myString[i])) {
+      count += 1;
+    }
+  }
+	return count;
+}
 			
-			
-			
-			function vowel_count(myString) {
-				var count = 0,
-				stringLength = myString.length;
-				vowels = /[aeiou]/i;
-				
-				for(var i = 0; i < stringLength; i += 1) {
-					if(vowels.test(myString[i])) {
-						count += 1;
-					}
-				}
-				return count;
-			}
-			
-			
-			
-			function reverse(someString) {
-				return someString.split('').reverse().join('');
-			}
+function reverse(someString) {
+  return someString.split('').reverse().join('');
+  }
